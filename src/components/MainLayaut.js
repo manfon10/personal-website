a3fb56eda359c777da87b18ddcc85891b95a30f5
Manuel Fonseca <manueldazafon@gmail.com>
2022-03-27 22:49:15 -0500

    delete import component Footer

--- src/components/MainLayaut.js
+++ src/components/MainLayaut.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
 import PersonalInfo from './PersonalInfo';
-import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 
 const MainLayaut = () => {
